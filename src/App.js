80a82c05d30778ffb85e6230e26c09f7dda294b4
usamamalik909 <malik.usama9100@gmail.com>
2023-09-01 14:02:24 +0500

    Counter App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
+  const [count,setCount] = useState(0);
+  const  increment =() => {
+    setCount(count+1);
+  }
+  const  decrement =() => {
+    setCount(count-1); 
+  }
+  const  reset =() => {
+    setCount( 0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container my5 text-center 
+    ">
+      <div ><h1>Counter App in React JS</h1>
+
+        <h2>The count is {count}</h2>
+        <button className="btn btn-success mx-3" onClick={increment}>
+                        Increment
+        </button> 
+        <button className="btn btn-dark mx-3" onClick={decrement} disabled = {count===0}>Decrement</button>
+        <button className="btn btn-warning mx-3" onClick= {reset} disabled = {count ===0}>Reset</button>
+      </div>
     </div>
   );
 }
